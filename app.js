eea7fcf56438d7db5dbaa1a1798463e7fd6ff08e
Rishabh Kumar <errk58676@gmail.com>
2020-12-24 18:27:54 +0530

    Adding changes related to port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
-//var PORT = process.env.PORT || 8000;
-var PORT = 8000;
+var PORT = process.env.PORT || 8000;
+//var PORT = 8000;
 var bodyparser = require("body-parser");
 app.use(express.static("public"));
 app.use(bodyparser.json());
