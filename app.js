a43dbf438d3b96121a8339150b79b954b1a732e0
Rishabh Kumar <errk58676@gmail.com>
2020-12-24 16:22:50 +0530

    updated

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var express = require("express");
 var app = express();
+var PORT = process.env.PORT || 8000;
 var bodyparser = require("body-parser");
-//app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
-//app.use(express.bodyParser());
 app.use(bodyparser.json());
 var Data = require("./mongo");
 
@@ -49,4 +48,6 @@ app.post("/update",async function(req,res){
     res.send("ok");
 })
 
-app.listen(8000);
+app.listen(PORT,function(){
+    console.log("listening to"+PORT);
+});
